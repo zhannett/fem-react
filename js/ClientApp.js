14eeb062f201ee91da43e124c50ab7b87a9df09f
zhannett <janet@hubub.com>
2017-04-20 20:32:26 -0400

    Initial commit

---
+++ js/ClientApp.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyTitle from './MyTitle'
+
+var div = React.DOM.div
+
+var MyTitleFactory = React.createFactory(MyTitle)
+
+var MyFirstComponent = React.createClass({
+  render: function () {
+    return (
+            div(null, [
+              MyTitleFactory({title: 'props are the best', color: 'peru'}),
+              MyTitleFactory({title: 'semicolons are the worst', color: 'mediumaquamarine'}),
+              MyTitleFactory({title: 'jklol its okay if you like semicolons', color: 'rebeccapurple'}),
+              MyTitleFactory({title: 'im out of ideas', color: 'deepskyblue'})
+            ])
+    )
+  }
+})
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
