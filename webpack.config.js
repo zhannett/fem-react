663e3ed8785e38e8693752ce300323300c4e17f3
zhannett <janet@hubub.com>
2017-04-20 20:45:35 -0400

    config updates

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
     module: {
         rules: [
             {
+                include: path.resolve(__dirname, 'js'),
                 test: /\.js$/,
                 loader: 'babel-loader'
             }
